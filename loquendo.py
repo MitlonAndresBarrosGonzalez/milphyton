2c16561c1847b248b198351abe2e5c498409de67
Milton Andres Barros Gonzalez <mitonandresbarrosgonzalez@gmail.com>
2026-08-26 10:34:36 -0500

    1.0.0

---
+++ loquendo.py
@@ -0,0 +1,9 @@
+print("¿Qué deseas hoy?")
+opcionesDeMenu = ["1.reservacion,2.menu,3.contacto,4.acerca de nosotros,5.ubicacion"]
+for Menu in opcionesDeMenu:print (opcionesDeMenu[0])
+print (len(opcionesDeMenu))
+for i, opcionesDeMenu in enumerate (opcionesDeMenu):
+    print(i, opcionesDeMenu)
+
+if opcionesDeMenu: list [0]
+    
